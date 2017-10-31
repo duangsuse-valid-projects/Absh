d8519e7b8a9ecc634c68509838c87f2290b64624
duangsuse <fedora-opensuse@outlook.com>
2017-10-31 14:37:08 +0800

    Stript unnecessary comments, supress warning

--- app/src/main/java/org/duangsuse/absh/MainActivity.java
+++ app/src/main/java/org/duangsuse/absh/MainActivity.java
@@ -1,5 +1,12 @@
 package org.duangsuse.absh;
 
+import android.os.Bundle;
 import android.app.Activity;
+import bsh.Interpreter;
 
-public class MainActivity extends Activity { }
+public class MainActivity extends Activity {
+    @Override protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        Interpreter bsh = new Interpreter();
+    }
+}
